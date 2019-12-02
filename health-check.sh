c5a39822008d260fa16b7c93d87dbfb0414a40b6
雷乘 <leicheng@91fd.tech>
2019-12-02 11:00:52 +0800

    add "return 0"

--- health-check.sh
+++ health-check.sh
@@ -2,5 +2,6 @@
 PORT=$(cat /app/APP_PORT)
 if [[ -n $PORT ]]; then
     echo "Error: miss app port file."
+    return 0
 fi
 ./wait-http.sh -u "http://localhost:$PORT/health" -t 100 -c '"status":"UP"'
