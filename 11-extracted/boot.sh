0ecfe1254c9bd48fe3cb4db3f26f9dba4c64c99c
leicheng <leicheng@91fd.tech>
2020-09-08 18:17:39 +0800

    ss -lpn 改为 ss -pn

--- 11-extracted/boot.sh
+++ 11-extracted/boot.sh
@@ -63,7 +63,7 @@ while :; do
     r=$RANDOM
     let PORT=RANDOM_DIFF%r+LOWERPORT
     # PORT="$(shuf -i $LOWERPORT-$UPPERPORT -n 1)"
-    ss -lpn | grep -q ":$PORT " || break
+    ss -pn | grep -q ":$PORT " || break
 done
 
 #echo "Random port: $PORT"
